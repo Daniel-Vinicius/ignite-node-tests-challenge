7d567e7716218657110d7a0b2e3285efcb4bc703
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-07-08 14:39:06 -0300

    Forum

---
+++ src/modules/users/tests/routes/users/CreateUser.spec.ts
@@ -0,0 +1,25 @@
+import request from "supertest";
+// import { Connection, createConnection } from "typeorm";
+
+import { app } from "../../../../../app";
+
+import { userFixture } from "../../fixtures/UserFixture";
+
+const user = userFixture();
+// let connection: Connection;
+
+describe("Create User Route", () => {
+  // beforeAll(async () => {
+  //   connection = await createConnection();
+  //   await connection.runMigrations();
+  // });
+
+  // afterAll(async () => {
+  //   await connection.dropDatabase();
+  //   await connection.close();
+  // });
+
+  it("should be able to create new user", async () => {
+    await request(app).post("api/v1/users").send(user).expect(201);
+  })
+})
